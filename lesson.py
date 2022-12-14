d45a0248349fbac2d9cbcf49b5b085d2e40a21f0
hirozo99 <hiro.1999@me.com>
2022-12-15 02:44:29 +0900

    add

--- lesson.py
+++ lesson.py
@@ -1,12 +1,3 @@
-#入力
-N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
+from termcolor import colored
 
-#答えを求める
-ans = 0
-for i in range(N):
-	ans += (1 / 3) * A[i] + (2 / 3) * B[i]
-
-#出力
-print("%.7f" % ans)
+print(colored("test", "red"))
